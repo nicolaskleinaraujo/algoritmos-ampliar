db7020ed16242e076ada6650e74a3509535a605e
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-05 22:42:50 -0300

    atividade 2

--- Semana 1/Atv2.c
+++ Semana 1/Atv2.c
@@ -4,20 +4,15 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-
-    // Declaração de variáveis do tipo float (real)
+int main(){
     float lado, area;
 
-    //Entrada de dados utilizando scanf
-    printf("Insira a medida do lado do quadrado: ");
+    printf("Informe o tamanho do lado do quadrado: ");
     scanf("%f", &lado);
 
-    // Processamento dos dados - Cálculo da área
     area = pow(lado, 2);
 
-    // Saída do resultado
-    printf("A area e: %.2f", area);
+    printf("Area do quadrado: %.2f", area);
 
     return 0;
 }
