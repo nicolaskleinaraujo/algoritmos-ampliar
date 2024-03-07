90d37f1cc98115d2506a853fe072f5b919f059fd
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-06 21:41:47 -0300

    atividade 5

---
+++ semana_2/atv_5.c
@@ -0,0 +1,21 @@
+/*
+    Escreva um codigo que solicite ao usuario que insira numeros positivos e calcule a soma desses numeros
+    até que ele insira um numero 0
+*/
+
+#include <stdio.h>
+
+int main(){
+    int num = 0;
+    int cont = 0;
+
+    do {
+        printf("Digite um numero que nn seja 0: ");
+        scanf("%d", &num);
+        cont += num;
+    } while (num != 0);
+
+    printf("%d", cont);
+
+    return 0;
+}
