ae91130a00d989732d2b70c37729d366f811aafd
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-06 21:20:03 -0300

    atividade 4

---
+++ semana_2/atv_4.c
@@ -0,0 +1,16 @@
+/*
+    Escreva um codigo que imprima na tela uma contagem regressiva de 10 a 0
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int cont;
+
+    for (cont = 10; cont >= 0; cont--) {
+        printf("%d\n", cont);
+    }
+
+    return 0;
+}
